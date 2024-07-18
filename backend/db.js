3defa7a578f2c8a43442f7a0560ae73f9a6c3f0b
sujalbindra1012 <sujalbindra@gmail.com>
2024-07-18 17:32:10 +0530

    database updated

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/?directConnection=true";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB successfully");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        // Exit process on connection failure, optionally you can handle this differently
+        process.exit(1);
+    }
+};
+
+module.exports = connectToMongo;
